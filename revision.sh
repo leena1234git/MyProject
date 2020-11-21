ec738c37abe80329ec3986ca2a2a86c8a5e65c87
leena1234gitgit <leenakeni1234@gmail.com>
2020-11-22 00:15:00 +0530

    Initial commit

---
+++ revision.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+numbers=$((RANDOM%7+1))
+numberOne=1
+numberTwo=2
+numberThree=3
+numberFour=4
+numberFive=5
+numberSix=6
+numberSeven=7
+
+
+case $numbers in
+   $numberOne)
+       echo "Its a Sunday"
+      ;;
+   $numberTwo)
+       echo "Its a Monday :("
+      ;;
+   $numberThree)
+       echo "Its a Tuesday :("
+      ;;
+ $numberFour)
+       echo "Its a Wednesday :("
+      ;;
+   $numberFive)
+       echo "Its a Thursday :("
+      ;;
+ $numberSix)
+       echo "Its a Friday :):("
+      
+      ;;
+   *)
+echo "Its a Saturday *** :
+     
+     ;;
+
+
+
